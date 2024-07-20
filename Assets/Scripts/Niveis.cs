fa4bb0dda015002974b88a68f4ef97657b109164
JohnBaska <131826014+JohnBaska@users.noreply.github.com>
2024-07-20 19:16:06 -0300

    teste

--- Assets/Scripts/Niveis.cs
+++ Assets/Scripts/Niveis.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Niveis : MonoBehaviour
 {
-    public string nivel;
-    // Start is called before the first frame update
-    public void acessarNivel()
+    public void acessarNivel(string nivel)
     {
-        
+        string scene = "nivel " + nivel;
+        SceneManager.LoadScene(scene);
     }
 }
